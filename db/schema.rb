e07c4436117a10fc51887e84f09f0f2dcd3af299
buknoy9135 <jalilanthony@gmail.com>
2025-08-27 12:22:47 +0800

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_26_104255) do
     t.jsonb "latest_event_raw"
     t.string "tracking_provider"
     t.string "carrier_code"
+    t.jsonb "full_payload", default: []
     t.index ["user_id", "tracking_number", "courier_name"], name: "index_packages_on_user_tracking_with_courier", unique: true, where: "(courier_name IS NOT NULL)"
     t.index ["user_id", "tracking_number"], name: "index_packages_on_user_tracking_without_courier", unique: true, where: "(courier_name IS NULL)"
     t.index ["user_id"], name: "index_packages_on_user_id"
